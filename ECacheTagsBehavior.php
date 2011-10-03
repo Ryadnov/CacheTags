beb30b53db647a5d2bcb3dc219d7826ee5cc0f75
Ryadnov <2008.big@gmail.com>
2011-10-03 08:32:26 +0400

    format

--- ECacheTagsBehavior.php
+++ ECacheTagsBehavior.php
@@ -1,18 +1,19 @@
 <?php
+
 class ECacheTagsBehavior extends CBehavior {
+	const PREFIX = '__tag__';
 
-const PREFIX = '__tag__';
+	/**
+	 * Инвалидирует данные, помеченные тегом(ами)
+	 *
+	 * @param $tags
+	 * @return void
+	 */
+	public function clear($tags)
+	{
+		foreach ((array) $tags as $tag) {
+			$this->owner->set(self::PREFIX . $tag, time());
+		}
+	}
 
-/**
- * Инвалидирует данные, помеченные тегом(ами)
- *
- * @param $tags
- * @return void
- */
- public function clear($tags) {
-
-foreach ((array)$tags as $tag) {
- $this->owner->set(self::PREFIX.$tag, time());
- }
- }
 }
--- ECacheTagsDependency.php
+++ ECacheTagsDependency.php
@@ -1,37 +1,46 @@
 <?php
+
 class ECacheTagsDependency implements ICacheDependency {
 
-protected $timestamp;
- protected $tags;
+	protected $timestamp;
+	protected $tags;
 
-/**
- * В качестве параметров передается список тегов
- *
- * @params tag1, tag2, ..., tagN
- */
- function __construct() {
- $this->tags = func_get_args();
- }
+	/**
+	 * В качестве параметров передается список тегов
+	 *
+	 * @params tag1, tag2, ..., tagN
+	 */
+	function __construct()
+	{
+		$this->tags = func_get_args();
+	}
 
-/**
- * Evaluates the dependency by generating and saving the data related with dependency.
- * This method is invoked by cache before writing data into it.
- */
- public function evaluateDependency() {
- $this->timestamp = time();
- }
+	/**
+	 * Evaluates the dependency by generating and saving the data related with dependency.
+	 * This method is invoked by cache before writing data into it.
+	 */
+	public function evaluateDependency()
+	{
+		$this->timestamp = time();
+	}
 
-/**
- * @return boolean whether the dependency has changed.
- */
- public function getHasChanged() {
- $tags = array_map(function($i) { return ECacheTagsBehavior::PREFIX.$i; }, $this->tags);
- $values = Yii::app()->cache->mget($tags);
+	/**
+	 * @return boolean whether the dependency has changed.
+	 */
+	public function getHasChanged()
+	{
+		$tags = array_map(function($i) {
+					return ECacheTagsBehavior::PREFIX . $i;
+				}, $this->tags);
+		$values = Yii::app()->cache->mget($tags);
 
-foreach ($values as $value) {
- if ((integer)$value > $this->timestamp) { return true; }
- }
+		foreach ($values as $value) {
+			if ((integer) $value > $this->timestamp) {
+				return true;
+			}
+		}
 
-return false;
- }
+		return false;
+	}
+
 }
